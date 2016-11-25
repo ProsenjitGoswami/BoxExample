519017027756e842485584b6c19062053859482b
Prsenjit Goswami <PG@macpcaim00160.local>
2016-11-25 13:37:53 +0530

    Initial Commit

---
+++ BoxExample/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BoxExample
+//
+//  Created by Prsenjit Goswami on 25/11/16.
+//  Copyright © 2016 Prosenjit Goswami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ BoxExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxExample
+//
+//  Created by Prsenjit Goswami on 25/11/16.
+//  Copyright © 2016 Prosenjit Goswami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
